b27e2a1cfa75f6fbdfecc76d1ca1d6f108d6f6fb
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-12-24 23:53:18 +0530

    initial commit

---
+++ src/test/java/com/barath/spring/samples/springprogramaticallyregisterbeans/SpringProgramaticallyRegisterBeansApplicationTests.java
@@ -0,0 +1,16 @@
+package com.barath.spring.samples.springprogramaticallyregisterbeans;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringProgramaticallyRegisterBeansApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
